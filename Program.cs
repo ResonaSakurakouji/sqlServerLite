628075280a4fbd01a00a1a18e63a37b3c5b88e74
QinRuiZheng <QinRuiZheng@QINRUIZHENG-L13>
2024-01-04 09:47:12 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+namespace sqlServerLite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 定义连接字符串
+            string connectionString = "Server=powerbi-prd,24333;Integrated Security=true;";
+
+            // 建立数据库连接
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    // 打开连接
+                    connection.Open();
+
+                    // 连接已打开，可以执行查询或其他操作
+                    Console.WriteLine("连接成功！");
+
+                    // 示例：执行查询
+                    string sqlQuery = "SELECT name FROM sys.databases";
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                for (int i = 0; i < reader.FieldCount; i++)
+                                {
+                                    Console.Write($"{reader[i],-20}");
+                                }
+                                Console.WriteLine();
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("连接失败: " + ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Str2Length.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqlServerLite
+{
+    public class Str2Length
+    {
+        public static bool StrIsNumberInt(string str)
+        {
+            try
+            {
+                Convert.ToInt32(str);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool StrIsNumberDouble(string str)
+        {
+            try
+            {
+                Convert.ToDouble(str);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static string Str2LengthCenter(string str, int length)
+        {
+            string result = "";
+            if (StrIsNumberInt(str))
+            {
+                string formatLength = $"{{0,{length}}}";
+                int resultInt = Convert.ToInt32(str);
+                result = string.Format(formatLength, resultInt);
+            }
+            else if (StrIsNumberDouble(str))
+            {
+                int spaces = length - str.Length;
+                result = str.PadLeft(length - spaces);
+            }
+            else
+            {
+                int spaces = length - str.Length;
+                for (int i = 0; i < str.Length; i += 1)
+                {
+                    char ch = str[i];
+                    if (!char.IsLetterOrDigit(ch))
+                    {
+                        spaces -= 1;
+                    }
+                }
+
+                result = spaces % 2 == 0
+                    ? str.PadLeft(length - spaces / 2)
+                    : str.PadLeft(length - spaces / 2 - 1);
+
+                result = result.PadRight(length);
+            }
+
+            return result;
+        }
+
+        public static string Str2LengthLeft(string str, int length)
+        {
+            string result = "";
+            if (StrIsNumberInt(str))
+            {
+                string formatLength = $"{{0,{length}}}";
+                int resultInt = Convert.ToInt32(str);
+                result = string.Format(formatLength, resultInt);
+            }
+            else if (StrIsNumberDouble(str))
+            {
+                int spaces = length - str.Length;
+                result = str.PadLeft(length - spaces);
+            }
+            else
+            {
+                int spaces = length - str.Length;
+                for (int i = 0; i < str.Length; i += 1)
+                {
+                    char ch = str[i];
+                    if (!char.IsLetterOrDigit(ch))
+                    {
+                        spaces -= 1;
+                    }
+                }
+
+                result = str.PadRight(length - spaces);
+            }
+
+            return result;
+        }
+
+        public static int GetStrLength(string str)
+        {
+            int result = str.Length;
+            foreach (char ch in str)
+            {
+                if (!char.IsLetterOrDigit(ch))
+                {
+                    result += 1;
+                }
+            }
+            return result;
+        }
+    }
+}
